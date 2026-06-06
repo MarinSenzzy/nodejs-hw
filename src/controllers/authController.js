4911e97e6ce78f429a7e74fb8d69a19701f1b153
MarinSenzzy <gwstudiogw48090@gmail.com>
2026-06-06 23:03:01 +0200

    fix message from reset password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -150,5 +150,5 @@ export const resetPassword = async (req, res) => {
   await User.updateOne({ _id: user._id }, { password: hashedPassword });
 
   await Session.deleteMany({ userId: user._id });
-  res.status(200).json({ message: 'Password reset email sent successfully' });
+  res.status(200).json({ message: 'Password reset successfully' });
 };
